b6ca90e6688706fe229520a0544105981090aba8
hejw123 <54312752+hejw123@users.noreply.github.com>
2020-06-26 21:53:54 +0800

    作业1:利用requests抓取猫眼top10影片

--- week01/requests_maoyan.com.py
+++ week01/requests_maoyan.com.py
@@ -69,6 +69,7 @@ def getMyInfo(agent, cookie, url) -> list :
     data.append(time)
     return data
 
+# 开始抓取
 print("start-time:"+time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 user_agent = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
 coookie = '__mta=174397341.1593166132263.1593166146560.1593166258587.3; uuid_n_v=v1; uuid=08940570B79511EAA1F189776DB49E05C2ADF24E320243268AAED2B04040EE34; _csrf=274090bc3afea611b5c6be428852fa61754c55fbfed87b7b8d2dc07e9323d1e3; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1593166132; _lxsdk_cuid=172f019f29bc8-0c195289997faa-143f6256-13c680-172f019f29bc8; _lxsdk=08940570B79511EAA1F189776DB49E05C2ADF24E320243268AAED2B04040EE34; mojo-uuid=e4a79b862f7ffbe071a73e14872d964c; mojo-session-id={"id":"dca07b1449c124511dfbb6ba8eb8d187","time":1593172637349}; mojo-trace-id=2; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1593172668; __mta=174397341.1593166132263.1593166258587.1593172668394.4; _lxsdk_s=172f07d36b0-c5a-882-9f2%7C%7C4'
